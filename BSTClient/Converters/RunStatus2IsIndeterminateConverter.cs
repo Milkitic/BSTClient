79d5ce93dc572b41ef5389b26449146a1f97568a
Milkitic <Milkitic@126.com>
2020-07-31 18:14:46 +0800

    client upload list

---
+++ BSTClient/Converters/RunStatus2IsIndeterminateConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BSTClient.Converters
+{
+    class RunStatus2IsIndeterminateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
