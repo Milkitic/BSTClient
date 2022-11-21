6956ce0f067c192da12caa6b691e53c127dac514
Milkitic <Milkitic@126.com>
2022-11-21 14:46:01 +0800

    Refactor

--- BSTClient/VmBase.cs
+++ BSTClient/VmBase.cs
@@ -1,5 +1,5 @@
-﻿using BSTClient.Annotations;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -15,10 +15,17 @@ namespace BSTClient
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
 
